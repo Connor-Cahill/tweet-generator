07fe5b4f717fb247603e1a56be1be3aea4ebc3b2
Connor Cahill <connorcahill913@gmail.com>
2019-01-27 14:40:46 -0800

    added dictionary_word.py file and setup function to return n amount of random words from word file.

---
+++ dictionary_words.py
@@ -0,0 +1,17 @@
+import random
+
+def words_from_dict(num_words):
+  '''
+Takes num words as argument and returns n random words from dictionary
+  '''
+  rand_word_arr = []
+  word_file = open('/usr/share/dict/web2', 'r').read().splitlines()
+  for i in range(num_words):
+    random_line = random.randint(0, 235885)
+    rand_word = random.choice(word_file)
+    rand_word_arr.append(rand_word)
+  return ' '.join(rand_word_arr)
+
+if __name__ == '__main__':
+  num_words = int(input('How many words in the sentence? '))
+  print(words_from_dict(num_words))

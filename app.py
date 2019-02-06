4192a06c9c88dcdc081fad2c5711faa2c2127000
Connor Cahill <connorcahill913@gmail.com>
2019-02-05 19:55:38 -0800

    [Fixed]: index now displays a random quote.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import histogram as h
+
 app = Flask(__name__)
 
 @app.route('/')
 def triggerSentence():
-  return 'Hello, World!'
+  my_dict = h.histogram('histo_text.txt')
+  sentence = h.generate_sentence(my_dict, 15)
+  return str(sentence)

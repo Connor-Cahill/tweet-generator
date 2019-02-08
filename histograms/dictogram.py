0a757be14373c144395002216567d660be01125a
Connor Cahill <connorcahill913@gmail.com>
2019-02-08 13:01:04 -0800

    [Added]: finished up most of dictogram class but need to test.

--- histograms/dictogram.py
+++ histograms/dictogram.py
@@ -11,11 +11,15 @@ class Dictogram(dict):
     self.tokens = 0 # Tokens are appearances of words (total_words)
     # * count words in the given text
     if text is not None:
-      pass #! Need to make a clean_up_text method to string what i need
+      words = self.clean_up_text(text)
+      for word in words:
+        self.add_to_count(word)
+    
 
   def add_to_count(self, word):
     ''' Checks if word is in dictogram and either adds it or adds to frequency count '''
     self.tokens += 1
+    print('Something is happening i promise')
     if word in self:
       self[word] += 1
     else:
@@ -29,4 +33,35 @@ class Dictogram(dict):
     else:
       return 0
   
+  def clean_up_text(self, text):
+    ''' Texts in text and removes special chars and newlines for dictogram '''
+    new_text = text.lower()
+    translator = str.maketrans('', '', string.punctuation)
+    ret = new_text.translate(translator)
+    ret = ret.split(' ')
+    return ret
   
+
+  def to_file(self, file):
+    ''' Takes dictogram and writes it to inputted file '''
+    with open(file, 'w+') as text_file:
+      text_file.write('\n****** NEW HISTOGRAM RESULTS *******\n')
+      for key, value in self.items():
+        text_file.write('{} : {}\n'.format(key, value))
+
+
+
+
+def main():
+  ''' runs some testing for Dictogram class '''
+  file = sys.argv[1]
+
+  my_dict = Dictogram(file)
+
+  my_dict.to_file('dicto_results.txt')
+
+
+if __name__ == "__main__":
+  main()
+
+

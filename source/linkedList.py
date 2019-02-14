6bdf8682d631dd7e1e4c2a5ce538b5df146b9704
Connor Cahill <connorcahill913@gmail.com>
2019-02-13 21:39:09 -0800

    [Implemented]: find, prepend, append methods for linked list class

--- source/linkedList.py
+++ source/linkedList.py
@@ -16,6 +16,7 @@ class LinkedList(object):
         """Initialize this linked list and append the given items, if any."""
         self.head = None  # First node
         self.tail = None  # Last node
+        self.size = 0
         # Append given items
         if items is not None:
             for item in items:
@@ -51,27 +52,52 @@ class LinkedList(object):
 
     def length(self):
         """Return the length of this linked list by traversing its nodes.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all nodes and count one for each
+        TODO: Running time: O(1) because we made size a property of the linked list class"""
+        return self.size
 
     def append(self, item):
         """Insert the given item at the tail of this linked list.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Create new node to hold given item
-        # TODO: Append node after tail, if it exists
+        TODO: Running time: O(1) because we have the tail pointer and just need to readjust 2 pointers"""
+        new_node = Node() # Creates new node
+        self.size += 1 #  increment size of linkedlist counter
+        new_node.data = item # gives new_node data of item (argument)
+        cur_last = self.tail  # stores value of current tail to cur_val
+        self.tail = new_node  # changes the tail to the new_node
+        cur_last.next = new_node  # adds the new_node to the cur_lasts next pointer
+
+        
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Create new node to hold given item
-        # TODO: Prepend node before head, if it exists
+        TODO: Running time: O(1) Because we have a head pointer so we only need to adjust 2 pointers"""
+        new_node = Node() # create new node
+        self.size += 1 #  increment size of linked list counter
+        new_node.data = item  # set nodes data to item
+        next_node = self.head # gets value of current first and stores as next_node
+        self.head = new_node  # sets the new_node as the head
+        new_node.next = next_node # sets the new_node's pointer towards the previous head (next_node)
 
     def find(self, quality):
         """Return an item from this linked list satisfying the given quality.
-        TODO: Best case running time: O(???) Why and under what conditions?
-        TODO: Worst case running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all nodes to find item where quality(item) is True
-        # TODO: Check if node's data satisfies given quality function
+        TODO: Best case running time: O(1) if the quality is the head or tail nodes data
+        TODO: Worst case running time: O(n) we have to loop through every node"""
+        if self.head.data == quality: # Check if the head node's data is what we are looking for 
+          return self.head.data # if it is return it and break the function
+        if self.tail.data == quality: # Same as above but checking tail pointer
+          return self.tail.data
+
+        current_node = self.head  # Assign first node to current_node var
+        for i in range(self.size):  # iterate over size of the linked list
+          if current_node.data == quality:  # if current nodes data equals quality, return it
+            return current_node.data
+          if current_node.next == None:  # if current_node.next = None, we have iterated over all items 
+            return None # Quality is not in linked list so we return none
+          current_node = current_node.next  # reassinging current_node to the next node in linked list
+
+          
+
+        
+
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.

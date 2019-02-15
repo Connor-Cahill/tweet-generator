4a94f41475c1bc2c672da7869a7b4f10c824685d
Connor Cahill <connorcahill913@gmail.com>
2019-02-15 13:37:24 -0800

    [Added]: linked list implementation of stack

--- source/stack.py
+++ source/stack.py
@@ -9,10 +9,10 @@ class Stack_Node(Node):
 
 class Stack:
   """ linked list stack implementation """
-  def __init__(self)
-  """ creates new instance of stack """
-  self.size = 0 # size counter for our stack (keeps length method constant time)
-  self.head = None  # first node set to none by default 
+  def __init__(self):
+    """ creates new instance of stack """
+    self.size = 0 # size counter for our stack (keeps length method constant time)
+    self.head = None  # first node set to none by default 
 
   def is_empty(self):
     """ returns bool whether stack is empty """
@@ -35,7 +35,41 @@ class Stack:
       self.head = pop_item.next # reassign head to old head's next node
       self.size -= 1  # decrement size counter
       return pop_item.data  # return data of popped item
-      
+    
+  def peek(self):
+    """ peek at data at top of stack """
+    return self.head.data # return head data
+  
+  def length(self):
+    """ returns size of stack """
+    return self.size  # size of stack
+  
+  def __str__(self):
+    """ prints string representation of stack """
+    ret_string = '' # create return string
+    node = self.head #  grab value of first node
+    while node is not None: # loop while node is not node
+      ret_string += '[ {} ]'.format(node.data) # append data to return string 
+      node = node.next  # reassign node to next node in stack
+    return ret_string
+
+def test_stack():
+  """ runs some test methods on a stack """
+  stack = Stack()
+  stack.push('A')
+  stack.push('B')
+  stack.push('C')
+  print(stack)
+  print('PEEK: ', stack.peek())
+  print('POP: ', stack.pop())
+  print(stack.head.data)
+
+
+if __name__ == "__main__":
+  test_stack()
+
+
+  
 
 
 

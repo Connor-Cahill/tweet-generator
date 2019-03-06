b67970544e0e5072d298252f851cad399f278850
Connor Cahill <connorcahill913@gmail.com>
2019-03-05 17:45:05 -0800

    [Fixes]: Client side JS call to api. Now returns tweet.

--- static/javascript/scripts.js
+++ static/javascript/scripts.js
@@ -1,9 +1,9 @@
 const contentArea = document.getElementById('quote');
 function setQuote() {
   axios.get('/tweets')
-  .then(res => res.json())
-  .then((tweet) => {
-    contentArea.innerHTML = tweet.tweet
+  .then((res) => {
+    contentArea.innerHTML = res.data.tweet;
+    // makes the tweet gen speak if uncommented
     // const utterance = new SpeechSynthesisUtterance(tweet.tweet);
     // window.speechSynthesis.speak(utterance);
   })

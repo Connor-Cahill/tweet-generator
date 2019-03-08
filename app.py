93a71c94b6e2acb5078c2c68632630443c46b571
Connor Cahill <connorcahill913@gmail.com>
2019-03-08 04:14:11 -0800

    got markov chain pickled. Need to figure out how to still parse sentence starters ...

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify, render_template
 from source.markov import Markov_Chain
+from markov import markov
 import pickle
+import json
 
 app = Flask(__name__)
 
 
 def serialize_markov(markov_chain, file):
   """Serializes a large markov chain to a file that can later be retrieved"""
-  with open(file) as cur_file:
-    pickle.dumps(markov_chain, cur_file)
+  with open(file, "wb") as cur_file:
+    pickle.dump(markov_chain, cur_file)
 
 
 def deserialize_markov(file):
   """Retrieves and deserializes markov chain from inputted file"""
-  with open(file) as cur_file:
+  with open(file, "rb") as cur_file:
     return pickle.load(cur_file)
 
 def generate_markov():
@@ -33,11 +35,10 @@ def index():
 @app.route('/tweets')
 def tweets():
     """Sends a json object containing a randomly generated tweet """
-    with open('review_text_file.txt') as file:
-        text = file.read()
-    markov_chain = Markov_Chain(text)
-    tweet = markov_chain.gen_sentence_2nd_order()
+    m_chain = Markov_Chain(None, markov) 
+    print(m_chain)
+    tweet = m_chain.gen_sentence_2nd_order()
     return jsonify({'tweet': tweet})
+if __name__ == "__main__":
+    mm = Markov_Chain(None, markov)
 
-if __name__ == '__main__':
-   generate_markov() 
---
+++ markov.py
@@ -0,0 +1,3 @@
+import pickle
+
+markov = pickle.load(open("markov.pickle", "rb")) 

6c274c065c19cb3a8c400961e76f2f28540c0f9e
Connor Cahill <connorcahill913@gmail.com>
2019-03-08 01:17:21 -0800

    removed unused text file

--- scrape.py
+++ scrape.py
@@ -1,3 +1,22 @@
 import pandas as pd
 import numpy
+import string
+import requests
+import bs4 as bs # beautiful soup for web scraping
 
+# list for appending links to
+link_list = []
+text_body = ''
+url = 'https://www.springfieldspringfield.co.uk/'
+html = requests.get('https://www.springfieldspringfield.co.uk/episode_scripts.php?tv-show=silicon-valley-2014')
+soup = bs.BeautifulSoup(html.text, 'html.parser')
+for link in soup.find_all('a', class_="season-episode-title"):
+    # grab the links from a tags
+    link_list.append(link.get('href'))
+    text = requests.get('{}{}'.format(url, link.get('href')))
+    transcripts = bs.BeautifulSoup(text.text, 'html.parser')
+    scripts = transcripts.find('div', class_="scrolling-script-container")
+    text_body += scripts.get_text()
+all_text = text_body.replace('-', '')
+with open('SV-text-file.txt', 'w+') as file:
+    file.write(all_text)
